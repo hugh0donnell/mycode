fe3d26f926257b5e696eaf7b8feedc34e10d4d5f
Hugh <ho3@ntrs.com>
2021-09-21 19:15:04 +0000

    lab 18

---
+++ iftest/condition03.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+hostname = input("What value should we set for hostname?")
+
+if hostname.upper() == "MTG":
+    print("The hostname was found to be " + hostname)
+    print("hostname matches expected config")
+
+## Always print out to the user
+print("Exiting the script")
+
