8731edc54c072838f1f9d8d87ca3e9883bb4064a
Hugh <ho3@ntrs.com>
2021-09-21 13:39:24 +0000

    test

---
+++ github/pushit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+
+   git commit in one script
+
+'''
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+import os
+
+commit_message  = input('Commit comment: ')
+working_dir     = 'cd ~/mycode'
+git_add         = 'git add *'
+git_commit      = 'git commit -m "' + commit_message + '"'
+git_push        = 'git push origin'
+
+os.system(working_dir)
+os.system(git_add)
+os.system(git_commit)
+os.system(git_push)
