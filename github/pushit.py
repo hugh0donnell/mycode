fffc3f2752243999debc0bd2c010b3fe72a5a2a3
Hugh <ho3@ntrs.com>
2021-09-21 20:13:29 +0000

    test

--- github/pushit.py
+++ github/pushit.py
@@ -8,12 +8,12 @@
 import os
 
 commit_message  = input('Commit comment: ')
-working_dir     = 'cd ~/mycode'
+working_dir     = '/home/student/mycode'
 git_add         = 'git add *'
 git_commit      = 'git commit -m "' + commit_message + '"'
 git_push        = 'git push origin'
 
-os.system(working_dir)
+os.chdir(working_dir)
 os.system(git_add)
 os.system(git_commit)
 os.system(git_push)
